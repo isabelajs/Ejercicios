72150cd37171b5de6b0a7419fbf6fbc549339cfc
isabelajs <isabelajimenezs96@gmail.com>
2019-10-20 23:22:51 -0500

    primer guardado

---
+++ ejercicios.py
@@ -0,0 +1,25 @@
+import turtle
+wn= turtle.Screen()
+wn.bgcolor("lightblue")
+
+isa = turtle.Turtle("turtle")
+isa.color("purple")
+
+
+
+#for i in range (3):cle
+    # isa.forward(100)
+    # isa.left(120)
+#for i in range (4):
+    # isa.forward(100) comento con control k+c quito k+u
+    # isa.left(90)
+# for i in range (8):
+#     isa.forward(100)
+#     isa.left(45)
+for i in range (6):
+    isa.forward(100)
+    isa.left(60)
+
+
+
+wn.exitonclick()
