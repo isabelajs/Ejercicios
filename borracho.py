facc4cbb6c0392906e93028c1e479f78b4736cbf
isabelajs <isabelajimenezs96@gmail.com>
2019-10-21 15:47:20 -0500

    hice un triangulo

--- borracho.py
+++ borracho.py
@@ -6,9 +6,12 @@ isa = turtle.Turtle("turtle")
 isa.fillcolor("purple")
 
 isa.forward(80)
-isa.left(90)
-isa.forward(120)
-isa.left(135)
-isa.forward(144)
+isa.left(113.5781)
+isa.forward(200)
+isa.left(132.8438)
+isa.forward(200)
+isa.left(113.5782)
+isa.forward(80)
+
 
 wn.exitonclick()
