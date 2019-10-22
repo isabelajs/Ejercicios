fc0c68d8321d1e22ad9e4c716ba9f003211c1cd3
isabelajs <isabelajimenezs96@gmail.com>
2019-10-21 23:32:31 -0500

    arregle el movimiento seno

--- movseno.py
+++ movseno.py
@@ -3,11 +3,13 @@ import turtle
 
 wn = turtle.Screen()
 wn.bgcolor('lightblue')
-wn.setworldcoordinates(2,0,360,2)
+wn.setworldcoordinates(0,-2,360,2)
 
 fred = turtle.Turtle()
+fred.goto(0,0)
 
 for angle in range(0,360,30):
+    
     y = math.sin(math.radians(angle))
     fred.goto(angle,y)
 
